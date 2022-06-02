17f3cda94de7afd6ae91bd23cfbd0120687424ea
anieto UNIR <anieto.unir@localhost.com>
2022-06-02 12:52:34 +0200

    Add Json Report

--- src/test/java/org/selenide/examples/cucumber/GoogleTest.java
+++ src/test/java/org/selenide/examples/cucumber/GoogleTest.java
@@ -3,12 +3,15 @@ package org.selenide.examples.cucumber;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.junit.TextReport;
 import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.rules.TestRule;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
+@CucumberOptions(plugin = { "json:target/cucumber.json"})
 public class GoogleTest {
   @Rule
   public TestRule report = new TextReport().onFailedTest(true).onSucceededTest(true);
